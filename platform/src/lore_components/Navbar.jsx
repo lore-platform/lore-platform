c9cc9fc3d5e5737adaa07a25fc006428af0ae659
Izzy Zennah <isabellezennah@gmail.com>
2024-02-07 22:40:06 +0000

    Created the statement to switch between

--- platform/src/lore_components/Navbar.jsx
+++ platform/src/lore_components/Navbar.jsx
@@ -1,26 +1,29 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { SearchBar } from "./SearchBar";
 import LoreLogo from "../assets/logo.png";
 import NavigationLink from "./NagivationLink";
 
-export const Navbar = () => {
-  return (
-    <>
-      <nav className="grid grid-cols-3 mb-3 justify-between items-center">
-        <Link to="/">
-          <img
-            className="min-w-0.5"
-            src={LoreLogo}
-            alt="LORE Company Logo"
-            id="loreLogo"
-          />
-        </Link>
-        <SearchBar />
-        <ul className="flex justify-end gap-4 place-content-center">
-          <NavigationLink to="/login" label="Login" />
-          <NavigationLink to="/signup" label="Signup" />
-        </ul>
-      </nav>
-    </>
-  );
+export const Navbar = ( { responsiveMode }) => {
+ return (
+   <>
+     {responsiveMode ? (
+       <nav className="grid grid-cols-3 mb-3 justify-between items-center">
+         <Link to="/">
+           <img
+             className="min-w-0.5"
+             src={LoreLogo}
+             alt="LORE Company Logo"
+             id="loreLogo"
+           />
+         </Link>
+         <SearchBar />
+         <ul className="flex justify-end gap-4 place-content-center">
+           <NavigationLink to="/login" label="Login" />
+           <NavigationLink to="/signup" label="Signup" />
+         </ul>
+       </nav>
+     ) : null}
+   </>
+ );
 };
